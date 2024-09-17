ccc71124053c6b2f4a0e3252c44cdca162d9e5ca
CayCall <131669034+CayCall@users.noreply.github.com>
2024-09-17 12:27:46 +0200

    started home page nav

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -1,4 +1,5 @@
 // need to get access to each page, get the index.html of each page
+//creating my menu items 
 const navMenuItems = [
     {name: "Home Page", href: "/index.html"},
     {name: "Services Page", href: "/index.html"},
@@ -6,9 +7,28 @@ const navMenuItems = [
     {name: "Design Page", href: "/index.html"}
 ]
 
-function(){
-    
+// method for creating Menu Items 
+function CreateMenuItems(){
+    const nav = document.querySelector('Nav-Bar');
+    const ul = document.createElement('ul');
+
+    // ensuring ther iteration of the Menu items array to create each individual element for the NAV
+    navMenuItems.forEach(element => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = item.name;
+        a.href = item.href;
+        li.appendChild(a)
+        ul.appendChild(li);
+    });
+    nav.appendChild(ul); 
 }
+
+//after html loads I will execute the DOM event 
+document.addEventListener('DOMContentLoaded', CreateMenuItems);
+
+
+
 
 
 
