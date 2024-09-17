64b3e51a04a941445895e503b8ae57107f80f4bb
CayCall <131669034+CayCall@users.noreply.github.com>
2024-09-17 11:11:41 +0200

    added pages and basic scripts

---
+++ Scripts/nav.js
@@ -0,0 +1,20 @@
+// need to get access to each page, get the index.html of each page
+const navMenuItems = [
+    {name: "Home Page", href: "/index.html"},
+    {name: "Services Page", href: "/index.html"},
+    {name: "Contact Page", href: "/index.html"},
+    {name: "Design Page", href: "/index.html"}
+]
+
+function(){
+    
+}
+
+
+
+
+   
+
+
+
+
