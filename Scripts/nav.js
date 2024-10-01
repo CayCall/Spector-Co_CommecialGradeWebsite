522d225aea9acf11055dbc27f68cdc41347dc154
CayCall <131669034+CayCall@users.noreply.github.com>
2024-10-01 13:34:17 +0200

    fixed  nav home adjusted  contact form  sizing

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -1,6 +1,6 @@
 // Creating menu items 
 const navMenuItems = [
-    { name: "Home", href: "./index.html" },
+    { name: "Home", href: "../../index.html" },
     { name: "About", href: "./Pages/AboutPage/index.html" },
     { name: "Services", href: "./Pages/ServicesPage/index.html" },
     { name: "Design", href: "./Pages/DesignPage/index.html" }
