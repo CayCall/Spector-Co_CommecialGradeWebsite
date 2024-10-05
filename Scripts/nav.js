ebed0f3ab7184000050b89cb6b5fb3be21958245
CayCall <131669034+CayCall@users.noreply.github.com>
2024-10-05 09:20:24 +0200

    changes

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -3,9 +3,9 @@
 // Creating menu items 
 const navMenuItems = [
     { name: "Home", href: "./index.html" },
-    { name: "About", href: "/Pages/AboutPage/index.html" },
-    { name: "Services", href: "/Pages/ServicesPage/index.html" },
-    { name: "Design", href: "/Pages/DesignPage/index.html" }
+    { name: "About", href: "Pages/AboutPage/index.html" },
+    { name: "Services", href: "Pages/ServicesPage/index.html" },
+    { name: "Design", href: "Pages/DesignPage/index.html" }
 ];
 
 // Method for creating Menu Items 
